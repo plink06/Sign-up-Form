40e8d41d918d0abd154104d15a47bc832d5013e4
plink06 <floch0674@proton.me>
2023-12-07 23:08:00 +0100

    validating password fields now works.

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const pass = document.querySelector("#pass");
-const confirm= document.querySelector("#confirm");
-const btn= document.querySelector("button-41");
+
+const btn = document.querySelector(".button-41");
 
 
-function checkpasswordmatch(confirm, pass){
-    if(confirm.innerText == pass.innerText){
-        return;
+
+btn.addEventListener("click",function(){
+    const pass = document.querySelector(".pas");
+    const confirm = document.querySelector(".conf");
+    if(pass.value=== confirm.value){
+        console.log("your passwords match wooo!!!");
     }else{
-        pass.classlist.add("nigger")
-        confirm.classlist.add("nigger")
+        console.log(pass.value);
+        console.log(confirm.value);
+        pass.classList.add("error");
+        confirm.classList.add("error");
     }
-    console.log(confirm.innerText)
-}
-btn.addEventListener("click",checkpasswordmatch(confirm,pass))
+})
